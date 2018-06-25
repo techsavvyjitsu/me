747b92fedf4ecd1aa12bcc08dbe98d0a22dadada
ashegde <ashegde@paypal.com>
2018-06-25 14:47:20 +0530

    refactor(layout): donation page

--- src/pages/donations/donations.js
+++ src/pages/donations/donations.js
@@ -10,34 +10,51 @@ class Donations extends Component {
   constructor(props) {
     super(props);
 
+    this.renderWebsites = this.renderWebsites.bind(this);
     this.renderList = this.renderList.bind(this);
   }
   
+  renderWebsites(items) {
+    return items.map((website) => {
+      const parseWebsiteLink = `https://${website.link}`;
+      return (
+        <li>{website.prefix}<ExtLink linkTo={parseWebsiteLink}>{website.link}</ExtLink></li>
+      );
+    });
+  }
+
   renderList(items) {
     return items.map((item) => {
-      let category = (
-        <li className="has-background-grey-lighter">
-          <ExtLink linkTo={item.linkTo}>{item.text}</ExtLink>
-        </li>
-      );
+      let labelWebsite = 'the website: ';
+      if (item.websites && item.websites.length > 1) {
+        labelWebsite = 'these websites: ';
+      }
 
-      return category;
+      return (
+        <div className="columns single-org-details">
+          <div className="column">
+            <h2 className="is-size-6 has-text-info has-text-weight-semibold">{item.title}</h2>
+            <p className="website is-size-6">Please visit {labelWebsite} for more information on donation: </p>
+            <ul className="is-size-6">{this.renderWebsites(item.websites)}</ul>
+          </div>
+          <div className="column">
+            <YouTube embedId={item.youtubeHash} />
+          </div>
+        </div>
+      );
     });
   }
 
   render() {
     return (  
       <section className="donations-component">
-        <Panel title="NLWS - Northern Lights Wildlife Shelter">
-          <div className="rows">
-            <div className="row">
-              <p className="website is-size-6">Visit Website: </p>
-              <ul className="is-12 is-size-6 parent-list">{this.renderList(orgs)}</ul>
-            </div>
+        <Panel title="Donations for Wildlife shelter, food supply, protection, etc.">
+          {this.renderList(orgs)}
+          {/*<div className="rows">
             <div className="row">
               <YouTube embedId="tPZzaPeZutw" />
             </div>
-          </div>
+    </div> */}
         </Panel>
       </section>
     );
