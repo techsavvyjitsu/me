7e6e74d7d7ea382e0677651634fec4db5a423f33
ashegde <ashegde@paypal.com>
2018-08-07 22:31:10 +0530

    update(components): error, home

--- src/pages/error/error.js
+++ src/pages/error/error.js
@@ -1,11 +1,15 @@
-// Packages
+import './error.less';
 import React, { Component } from 'react';
 
 class Error extends Component {
   render() {
     return (  
       <section className="error-component">
-        <h1>Error Page ... Coming Soon</h1>
+        <img src="http://res.cloudinary.com/hegdeashwin/image/upload/s---7pBjw6f--/c_scale,w_1920/v1533496824/animal-avian-bird-1149816.jpg" alt="404" />
+        <div className="content"> 
+          <h2 className="is-size-4">We couldn’t find what you were looking for.</h2>
+          <p className="is-size-6">Unfortunately the page you were looking for could not be found. It may be temporally unavailable, moved or no longer exist.</p>
+        </div>
       </section>
     );
   }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,48 +2,81 @@ import './home.less';
 import React, { Component } from 'react';
 
 // Components
-import Hero from '../../components/hero';
-import { Card, CardMedia, CardContent } from '../../components/card';
+import Icon from '../../components/icon';
 import { Link } from 'react-router';
+// import Hero from '../../components/hero';
+// import { Card, CardMedia, CardContent } from '../../components/card';
+// import { Link } from 'react-router';
+// import Carousel from '../../components/carousel';
 
 class Home extends Component {
   render() {
     return (  
-      <section className="home-component">
-        <Hero />
-
-        <div className="columns features-access has-text-centered">
-          <div className="column">
-            <Card>
-              <CardMedia classes="opensource" type="icon" source="osi" title="Open Source Projects" />
-              <CardContent title="Open Source Projects">
-                <p>Here are some of my exciting open source projects, learn and contribute</p>
-                <Link to="work/projects" className="button is-warning">Browse all Projects</Link>
-              </CardContent>
-            </Card>
+      <section className="home-component container">
+        
+        <div className="row columns">
+          <div class="column is-one-third">
+            <div class="card large">
+              <div class="card-image">
+                <figure class="image">
+                  <img src="http://res.cloudinary.com/hegdeashwin/image/upload/s--Qfrn_92a--/c_scale,q_100,w_1024/v1533409409/business-computer-contemporary-669996.jpg" alt="Tech Blogs and Articles" />
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-content has-text-centered">
+                    <p class="title is-4 no-padding">Tech Blogs and Articles</p>
+                  </div>
+                </div>
+                <div class="content has-text-centered">
+                  <p>Collection of tech articles and blogs</p>
+                  <Link to="work/articles" className="button is-warning">Browse all Articles</Link>
+                </div>
+              </div>
+            </div>
           </div>
 
-          <div className="column">
-            <Card>
-              <CardMedia type="icon" source="newspaper" title="Tech Blogs and Articles" iconType="far" />
-              <CardContent title="Tech Blogs & Articles">
-                <p>Here are some of my collection of tech articles and blogs</p>
-                <Link to="work/articles" className="button is-warning">Browse all Tech Articles</Link>
-              </CardContent>
-            </Card>
+          <div class="column is-one-third">
+            <div class="card large">
+              <div class="card-image">
+                <figure class="image">
+                  <img src="http://res.cloudinary.com/hegdeashwin/image/upload/s--zSHrXUAi--/c_scale,w_1024/v1533409411/code-coding-computer-574071_1.jpg" alt="Open Source Projects" />
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-content has-text-centered">
+                    <p class="title is-4 no-padding">Open Source Projects</p>
+                  </div>
+                </div>
+                <div class="content has-text-centered">
+                  <p>I tend to code software from scratch for web and mobile apps</p>
+                  <Link to="work/projects" className="button is-warning">Browse all Projects</Link>
+                </div>
+              </div>
+            </div>
           </div>
 
-          {/*<div className="column">
-            <Card>
-              <CardMedia classes="youtube" type="icon" source="youtube" title="Learn through Videos" iconType="fab" />
-              <CardContent title="Tech Blogs & Articles">
-                <p>Here are some of my collection of tech video series on YouTube</p>
-                <Link to="work/videos" className="button is-warning">Browse all Tech Videos</Link>
-              </CardContent>
-            </Card>
-          </div> */}
-
-          
+          <div class="column is-one-third">
+            <div class="card large">
+              <div class="card-image">
+                <figure class="image">
+                  <img src="http://res.cloudinary.com/hegdeashwin/image/upload/s--dkrDRepK--/c_scale,w_1024/v1533409409/camera-dslr-landscape-212372.jpg" alt="Wildlife, Nature and Landscape Photography" />
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-content has-text-centered">
+                    <p class="title is-4 no-padding">Photography</p>
+                  </div>
+                </div>
+                <div class="content has-text-centered">
+                  <p>Collection of photographs</p>
+                  <Link to="hobby/photography" className="button is-warning">Browse all Photos</Link>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
 
       </section>
